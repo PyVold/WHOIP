b5e4eb7f01991a6e41a5866f31300ac5a86ec019
PyVold <osama@ipdevops.com>
2022-02-20 14:49:12 +0000

    rollback function

--- access/delete.py
+++ access/delete.py
@@ -17,3 +17,15 @@ def delete_app(app_id):
         this_app.state='deletion requested'
         db.session.commit()
     return redirect(url_for('access.tokens'))
+
+
+@deletes.route('/rollback/<int:app_id>')
+@login_required
+def rollback_app(app_id):
+    this_app = Application.query.filter_by(id = app_id).first()
+    if app_id == 0 or not this_app :
+        return redirect(url_for('access.tokens'))
+    if this_app.user_id == current_user.id:
+        this_app.state='waiting approval'
+        db.session.commit()
+    return redirect(url_for('access.tokens'))
